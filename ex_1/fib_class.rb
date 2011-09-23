6722c6ad2596c37cd6040aa2c564686fa63d022f
Wayne Graham <wayne.graham@virginia.edu>
2011-09-23 10:12:32 -0300

    Edited ex_1/fib_class.rb via GitHub

--- ex_1/fib_class.rb
+++ ex_1/fib_class.rb
@@ -1,23 +1,39 @@
 # An incredibly idiotic way of solving this problem using classes
 
+# Always think about how other programmmers will use the classes and methods when you are naming Objects and methods
+
 class Fibo
+
+  # I think we only mentioned this in passing, but attr_accessor exposes the attributes to the class
+  # e.g sequence = Fibo.new(50).max
+  attr_accessor :max
+  attr_accessor :fib
+  attr_accessor :even
+  attr_accessor :sum
+
   def initialize(max)
     @max = max
-    @fib = [0,1]
+    @fib = [0,1] 
     @even = []
     @sum = 0
   end
 
+  # Think about the name of this method. Stack will probably confuse folks who have done anything
+  # with programming structures as it is a LIFO with some very specific behaviors (e.g. top, remove, 
+  # isempty).  
   def stack
     while @fib[-1] < @max
       @fib.push(@fib[-1] + @fib[-2])
     end
+    
+    # Will this code ever be reached? Should only get here if there is an error
     if @fib[-1] > @max
       @fib.pop
     end
     @fib
   end
 
+  # You're right on the cusp of recursion (a good thing).
   def pair
     @fib.each do |val|
       if val.remainder(2) == 0
@@ -42,6 +58,8 @@ class Fibo
     @max.to_s
   end
 end
+
+# This is pretty good. 
 
 puts "Enter a number below:"
 max = gets.chomp
